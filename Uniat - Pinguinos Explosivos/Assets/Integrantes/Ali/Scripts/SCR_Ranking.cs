0effb429e92c75aec4f982ec6fef92bb4c623a1d
AliceBearmaid <31712870+AliceBearmaid@users.noreply.github.com>
2018-03-23 12:40:31 -0600

    Ranking kinda

--- Uniat - Pinguinos Explosivos/Assets/Integrantes/Ali/Scripts/SCR_Ranking.cs
+++ Uniat - Pinguinos Explosivos/Assets/Integrantes/Ali/Scripts/SCR_Ranking.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class SCR_Ranking : MonoBehaviour {
 
     public GameObject[] players;
-    public GameObject[] checkPoint;
     SCR_PlayerTempStats[] myStats;
+    List<SCR_PlayerTempStats> mySortingList;
     void Start()
     {
         
@@ -17,14 +17,21 @@ public class SCR_Ranking : MonoBehaviour {
         for(int i =0; i<7;i++)
         {
             myStats[i] = players[i].GetComponent<SCR_PlayerTempStats>();
-
+            myStats[i].distanceToNext = Vector3.Distance(players[i].transform.position, myStats[i].nextTarget.transform.position);
+            mySortingList.Add(myStats[i]);
             
         }
-
+        mySortingList.Sort(SortByVar);
+        
 
         yield return new WaitForSeconds(0.1f);
 
         StartCoroutine(CheckPositions());
+    }
+
+    public int SortByVar(SCR_PlayerTempStats a, SCR_PlayerTempStats b)
+    {
+        return a.distanceToNext.CompareTo(b.distanceToNext);
     }
 
 
